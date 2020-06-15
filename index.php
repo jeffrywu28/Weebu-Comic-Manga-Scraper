c6fed68d355d62cd8f25c0c088a70d8a22e73191
adhi-wisesa <62042683+adhi-wisesa@users.noreply.github.com>
2020-06-15 16:37:45 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ $html = file_get_html('https://m.mangabat.com/manga-list-all?type=topview');
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -16,67 +17,79 @@ $html = file_get_html('https://m.mangabat.com/manga-list-all?type=topview');
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
   <style>
-    body{
+    body {
       margin: 0 px;
+      background-color: black;
     }
+
     .card {
-      margin: 0 auto; /* Added */
-      float: none; /* Added */
-      margin-bottom: 10px; /* Added */
+      margin: 0 auto;
+      /* Added */
+      float: none;
+      /* Added */
+      margin-bottom: 10px;
+      /* Added */
     }
-    .card-body > img {
-    max-height: 40%;
-    max-width: 40%;
-}
+
+    .card-body>img {
+      max-height: 40%;
+      max-width: 40%;
+    }
+
+    #active-page {
+      font-weight: bold;
+    }
   </style>
 </head>
-<body style="background-color: black;">
-<!--NavBar-->
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="navbar-brand" href="index.php">Home</a>
-      </li>
-      <li class="nav-item">
-        <a class="navbar-brand" href="#">Genre</a>
-      </li>
-    </ul>
-  </div>
-  <form class="form-inline my-2 my-lg-0">
-    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-  </form>
-</div>
-</nav>
 
-<!--Isi memakai card dalam container-->
-<div class="container-fluid" style="padding: 0 20% 0 20%;">
-  <?php
-  $count=1;
-  echo '<div class="row" style="margin-top: 1em;" style="margin: auto;">';
-  foreach($html->find('a.item-img') as $element){
-    ?>
-    <div class="col-md-3">
-      <div class="card text-center" style="width: 200px;">
-      <a class="card-block stretched-link text-decoration-none" href="manga.php?manga=<?php echo $element->href; ?>">
-      <img src="<?php echo $element->children(0)->src;?>" class="card-img-top">
-      <h5><?php echo $element->title;?></h5>
-      </a>
-      </div>
+<body>
+  <!--NavBar-->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="navbar-brand" href="index.php" id="active-page">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="navbar-brand" href="#">Genre</a>
+        </li>
+      </ul>
     </div>
-  
-    <?php
-    if($count%4 ==0){
-     echo '</div><div class="row" style="margin-top: 1em;" style="margin: auto;">';
-   }
-   $count++; 
- }
- ?>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+    </div>
+  </nav>
 
-</div>
+  <!--Isi memakai card dalam container-->
+  <div class="container-fluid" style="padding: 0 20% 0 20%;">
+    <?php
+    $count = 1;
+    echo '<div class="row" style="margin-top: 1em;" style="margin: 0 auto;">';
+    foreach ($html->find('a.item-img') as $element) {
+    ?>
+      <div class="col-md-3" style="margin: 0 auto;">
+        <div class="card text-center" style="width: 200px; margin: 0 auto;">
+          <a class="card-block stretched-link text-decoration-none" style="margin: 0 auto;" href="manga.php?manga=<?php echo $element->href; ?>" >
+            <img src="<?php echo $element->children(0)->src; ?>" class="card-img-top">
+            <h5><?php echo $element->title; ?></h5>
+          </a>
+        </div>
+      </div>
+
+    <?php
+      if ($count % 4 == 0) {
+        echo '</div><div class="row" style="margin-top: 1em;" style="margin: auto;">';
+      }
+      $count++;
+    }
+    ?>
+
+  </div>
 </body>
+
 </html>
