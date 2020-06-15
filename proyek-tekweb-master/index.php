c846a27ccf0a9aa1ae8db2c5b4613f792cd08774
adhi-wisesa <62042683+adhi-wisesa@users.noreply.github.com>
2020-06-15 16:37:15 +0700

    Add files via upload

---
+++ proyek-tekweb-master/index.php
@@ -0,0 +1,95 @@
+<?php
+include('simple_html_dom.php');
+$html = file_get_html('https://m.mangabat.com/manga-list-all?type=topview');
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Weebu Comic</title>
+  <!-- CSS only -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+  <!-- JS, Popper.js, and jQuery -->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+  <style>
+    body {
+      margin: 0 px;
+      background-color: black;
+    }
+
+    .card {
+      margin: 0 auto;
+      /* Added */
+      float: none;
+      /* Added */
+      margin-bottom: 10px;
+      /* Added */
+    }
+
+    .card-body>img {
+      max-height: 40%;
+      max-width: 40%;
+    }
+
+    #active-page {
+      font-weight: bold;
+    }
+  </style>
+</head>
+
+<body>
+  <!--NavBar-->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="navbar-brand" href="index.php" id="active-page">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="navbar-brand" href="#">Genre</a>
+        </li>
+      </ul>
+    </div>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+    </div>
+  </nav>
+
+  <!--Isi memakai card dalam container-->
+  <div class="container-fluid" style="padding: 0 20% 0 20%;">
+    <?php
+    $count = 1;
+    echo '<div class="row" style="margin-top: 1em;" style="margin: 0 auto;">';
+    foreach ($html->find('a.item-img') as $element) {
+    ?>
+      <div class="col-md-3" style="margin: 0 auto;">
+        <div class="card text-center" style="width: 200px; margin: 0 auto;">
+          <a class="card-block stretched-link text-decoration-none" style="margin: 0 auto;" href="manga.php?manga=<?php echo $element->href; ?>" >
+            <img src="<?php echo $element->children(0)->src; ?>" class="card-img-top">
+            <h5><?php echo $element->title; ?></h5>
+          </a>
+        </div>
+      </div>
+
+    <?php
+      if ($count % 4 == 0) {
+        echo '</div><div class="row" style="margin-top: 1em;" style="margin: auto;">';
+      }
+      $count++;
+    }
+    ?>
+
+  </div>
+</body>
+
+</html>
---
+++ proyek-tekweb-master/manga.php
@@ -0,0 +1,152 @@
+<?php
+include('simple_html_dom.php');
+$html = file_get_html($_GET['manga']);
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Weebu Comic</title>
+    <script>
+    </script>
+    <style>
+        body {
+            margin: 0 px;
+        }
+
+        .card {
+            margin: 0 auto;
+            /* Added */
+            float: none;
+            /* Added */
+            margin-bottom: 10px;
+            /* Added */
+        }
+
+        .card-body>img {
+            max-height: 40%;
+            max-width: 40%;
+        }
+
+        .chaptername {
+            float: left;
+        }
+
+        .uploaded {
+            float: right;
+        }
+
+        td {
+            overflow: hidden;
+            padding: 0;
+        }
+
+        #fixed_header {
+            display: block;
+            width: 100%;
+            overflow: auto;
+            height: 500px;
+        }
+    </style>
+</head>
+
+<body style="background-color: black;">
+    <!--NavBar-->
+    <!--NavBar-->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="navbar-brand" href="index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="navbar-brand" href="#">Genre</a>
+                </li>
+            </ul>
+        </div>
+        <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+        </div>
+    </nav>
+
+    <!-- main page -->
+    <div style="margin: 5em;">
+        <div class="card">
+            <div class="row no-gutters">
+                <div class="col-md-auto" style="width: 200px; margin-top: 1em; margin-left: 1em;">
+                    <span><img src="<?php echo $html->find('div.story-info-left', 0)->first_child()->first_child()->src; ?>" class="card-img" alt="..." style="width: 200px;"></span>
+                </div>
+                <div class="col-md-auto">
+                    <?php foreach ($html->find('div.story-info-right') as $element) { ?>
+                        <div class="card-body">
+                            <h3 class="card-title"><?php echo $element->children(0)->plaintext; ?></h3>
+                            <table>
+                                <tr>
+                                    <th>Author(s): <?php echo $element->children(1)->first_child()->children(1)->last_child()->plaintext; ?>
+                                    <th>
+                                </tr>
+                                <tr>
+                                    <th>Status : <?php echo $element->children(1)->first_child()->children(2)->last_child()->plaintext; ?>
+                                    <th>
+                                </tr>
+                                <tr>
+                                    <th>Genres: <?php echo $element->children(1)->first_child()->children(3)->last_child()->plaintext; ?>
+                                    <th> <?php }
+                                        foreach ($html->find('div.story-info-right-extent') as $element) { ?>
+                                </tr>
+                                <tr>
+                                    <th>First Chapter: <?php echo $element->children(2)->last_child()->plaintext; ?>
+                                    <th>
+                                </tr>
+                                <tr>
+                                    <th>Last Chapter: <?php echo $element->children(3)->last_child()->plaintext; ?>
+                                    <th>
+                                </tr> <?php } ?>
+                            </table>
+                        </div>
+                </div>
+            </div>
+            <div class="col" style="margin-top: 1em; margin-bottom: 1em; text-align: center;">
+                <button type="button" class="btn btn-info" data-toggle="collapse" data-target="#demo">Summary</button>
+                <div id="demo" class="collapse" style="text-align: left;">
+                    <?php echo $html->find('div#panel-story-info-description', 0); ?>
+                </div>
+            </div>
+        </div>
+        <div class="card" style="margin-top: 10px;" id="fixed_header">
+            <div class="card-body">
+                <div style="border-bottom: 5px solid red;" id="chapter_name">
+                    <span>Chapter Name</span> <span style="float: right;">Uploaded</span>
+                </div>
+                <table class="table table-borderless">
+                    <tbody>
+                        <?php
+                        foreach ($html->find('ul.row-content-chapter li') as $chapter) {
+                            echo '<tr> 
+                            <td class="chaptername">' . $chapter->find('a', 0)->plaintext . '</td>
+                            <td class="uploaded">' . $chapter->find('span', 0)->plaintext . '</td>
+                            </tr>';
+                        }
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+
+
+</body>
+
+</html>
