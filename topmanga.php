bb6fab240658f8a42fec3f79298355a1cdfe0a7e
jeffrywu28 <jefgu28@gmail.com>
2020-06-18 09:13:24 +0700

    add list top manga

---
+++ topmanga.php
@@ -0,0 +1,6 @@
+<?php
+include('simple_html_dom.php');
+$html = file_get_html("https://myanimelist.net/topmanga.php?type=favorite");
+foreach ($html->find('div.pb12 table.top-ranking-table tr.ranking-list td.title div.detail') as $element) {
+    echo $element->plaintext."<br>";
+    }
